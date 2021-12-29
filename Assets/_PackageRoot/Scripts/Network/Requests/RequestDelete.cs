ab0c5dc8415882bb2fa50a1351303892dd50cdd0
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2021-12-30 00:57:18 +0200

    Initial commit

---
+++ Assets/_PackageRoot/Scripts/Network/Requests/RequestDelete.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Networking;
+
+namespace Network.Extension
+{
+	public abstract class RequestDelete<T> : Request<T>
+	{
+		public override string RESTMethod => UnityWebRequest.kHttpVerbDELETE;
+
+		public RequestDelete(NetworkSO network) : base(network) { }
+
+		protected override UnityWebRequest CreateUnityWebRequest(string endpoint)
+		{
+			return UnityWebRequest.Delete(endpoint);
+		}
+	}
+}
